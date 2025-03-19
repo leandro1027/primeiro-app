92f6e6525558bbb9a0e4c07f2e6d15de425ed557
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2025-03-19 20:06:51 -0300

    .

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('novarota')
+  getNewRoute(){
+    return "Essa é uma nova rota!"
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,5 +5,6 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+  
 }
  
