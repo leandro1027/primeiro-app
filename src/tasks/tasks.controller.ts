4a51f277bdedad108360c67e81ab3a5ad0a11c35
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2025-03-19 21:48:59 -0300

    Update tasks.controller.ts

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+
+    @Get()
+    FindAllTasks(){
+        return('Buscando todas as rotas!')
+    }
+
+
+}
