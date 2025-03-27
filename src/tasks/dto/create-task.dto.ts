36643c236bfb2714138f18cc5bd4ee4e7d860e7b
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2025-03-26 21:47:04 -0300

    Update create-task.dto.ts

---
+++ src/tasks/dto/create-task.dto.ts
@@ -0,0 +1,16 @@
+/*
+DTO (Data Transfer Object) - Objeto de Tranferencia de Dados 
+Objetivo: Validar dados, transformar.
+É utilizado para representar quais dados e em que formatos uma determinada camada aceita e trabalha
+*/
+
+export class CreateTaskDto{
+
+}
+
+/*
+    id: number
+    name: string
+    description: string
+    completed: boolean
+*/
