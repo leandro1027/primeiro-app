e361029212a349a16b75ccbaaf3b722871a7fda0
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2025-03-31 10:08:56 -0300

    Update guests.controller.ts

--- src/guests/guests.controller.ts
+++ src/guests/guests.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { GuestsService } from './guests.service';
+import { CreateGuestDto } from './dto/create-guest.dto';
+import { UpdateGuestDto } from './dto/update-guest.dto';
 
 @Controller('guests')
 export class GuestsController {
@@ -14,6 +16,19 @@ export class GuestsController {
     findOneGuests(@Param('id') id: string){
         return this.GuestsService.findOne(id)
     }
+
+    @Post()
+    createGuests(@Body() CreateGuestDto: CreateGuestDto){
+        return this.GuestsService.create(CreateGuestDto)
+    }
+
+    @Patch(':id')
+        updateGuest(@Param('id') id: string, @Body() UpdateGuestDto: UpdateGuestDto){
+            return this.GuestsService.update(id, UpdateGuestDto)
+    }
+
+    @Delete(':id')
+    
 
     
 }
