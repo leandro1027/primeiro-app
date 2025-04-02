eecb8ba6d8604e2f8973420d02c2da848fb077c1
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2025-04-02 19:55:23 -0300

    Validator

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -4,15 +4,19 @@ Objetivo: Validar dados, transformar.
 É utilizado para representar quais dados e em que formatos uma determinada camada aceita e trabalha
 */
 
+import { MESSAGES } from "@nestjs/core/constants"
+import { IsNotEmpty, IsOptional, IsString, MaxLength, MinLength } from "class-validator"
+
 export class CreateTaskDto{
+    @IsString({message: "O nome precisa ser um texto!"})
+    @MinLength(5, {message: "O nome precisa ter no minimo 5 caracteres!"})
+    @MaxLength(20, {message: "O nome precisa ter no máximo 20 caracteres!"})
+    @IsNotEmpty({message: "O nome não pode ser vazio!"})
     readonly name: string
+
+    @IsString({message: "A descrição precisa ser um texto!"})
+    @IsNotEmpty({message: "A descrição não pode ser vazia!"})
+    @MaxLength(100, {message: "A descrição precisa ter no máximo 100 caracteres!"})
     readonly description: string
 
 }
-
-/*
-    id: number
-    name: string
-    description: string
-    completed: boolean
-*/
--- src/tasks/dto/update-task.dto.ts
+++ src/tasks/dto/update-task.dto.ts
@@ -1,7 +1,20 @@
+import { IsBoolean, IsNotEmpty, IsOptional, IsString, MaxLength, MinLength } from "class-validator"
 
 export class UpdateTaskDto{
+
+    @IsString({message: "O nome precisa ser um texto! "})
+    @MinLength(5, {message: "O nome precisa ter no minimo 5 caracteres!"})
+    @MaxLength(20, {message: "O nome precisa ter no máximo 20 caracteres!"})
+    @IsOptional()
     readonly name?: string
+
+    @IsString({message: "A descrição precisa ser um texto!"})
+    @IsNotEmpty({message: "A descrição não pode ser vazia!"})
+    @MaxLength(100, {message: "A descrição precisa ter no máximo 100 caracteres!"})
     readonly description?: string
+
+    @IsBoolean()
+    @IsOptional()
     readonly completed?: boolean
 
 }
