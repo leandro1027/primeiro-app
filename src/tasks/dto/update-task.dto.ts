38b9c8c7ccd035ac67bca28b5011d08221457223
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2025-04-02 20:00:14 -0300

    .

--- src/tasks/dto/update-task.dto.ts
+++ src/tasks/dto/update-task.dto.ts
@@ -1,17 +1,7 @@
 import { IsBoolean, IsNotEmpty, IsOptional, IsString, MaxLength, MinLength } from "class-validator"
+import { CreateTaskDto } from "./create-task.dto"
 
-export class UpdateTaskDto{
-
-    @IsString({message: "O nome precisa ser um texto! "})
-    @MinLength(5, {message: "O nome precisa ter no minimo 5 caracteres!"})
-    @MaxLength(20, {message: "O nome precisa ter no máximo 20 caracteres!"})
-    @IsOptional()
-    readonly name?: string
-
-    @IsString({message: "A descrição precisa ser um texto!"})
-    @IsNotEmpty({message: "A descrição não pode ser vazia!"})
-    @MaxLength(100, {message: "A descrição precisa ter no máximo 100 caracteres!"})
-    readonly description?: string
+export class UpdateTaskDto extends CreateTaskDto{
 
     @IsBoolean()
     @IsOptional()
