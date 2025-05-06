b548be8606be684c8dea978061dc4757eaba05e8
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2025-05-06 20:09:55 -0300

    .

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,15 +2,18 @@ import { Global, Module } from '@nestjs/common';
 import { hasSubscribers } from 'diagnostics_channel';
 import { HashingServiceProtocol } from './hash/hashing.service';
 import { BcryptService } from './hash/bcrypt.service';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
 @Global()
 @Module({
     providers: [{
         provide: HashingServiceProtocol,
         useClass: BcryptService
-    }],
+    }, AuthService],
     exports:[
         HashingServiceProtocol
-    ]
+    ],
+    controllers: [AuthController]
 })
 export class AuthModule {}
